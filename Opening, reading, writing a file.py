a315e306865ac06acc7cdb9ad776f04febdf273f
Kirja <kirja-87@mail.ru>
2023-07-31 11:17:26 +0400

    Task 2

--- Opening, reading, writing a file.py
+++ Opening, reading, writing a file.py
@@ -15,3 +15,17 @@ with open('cook_book.txt','r',encoding='utf-8') as f:
         blank_line = f.readline()
         cook_book.update(dish)
 print(f'cook_book = {cook_book}')
+print()
+
+result = {}
+def get_shop_list_by_dishes(dishes, person_count):
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredients in cook_book[dish]:
+                if ingredients['ingredient_name'] in result:
+                    result[ingredients['ingredient_name']]['quantity'] += int(ingredients['quantity']) * person_count
+                else:
+                    result[ingredients['ingredient_name']] = {'measure': ingredients['measure'], 'quantity': int(ingredients['quantity']) * person_count}
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(result)
